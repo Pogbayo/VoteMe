b6aa54bf16ca4f30fdf1c9f083aa6727c541db96
Pogbayo <adebayooluwasegun335@gmail.com>
2026-04-08 09:57:45 +0100

    Move secrets to appsettings.Local.json

---
+++ VoteMe.API/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace VoteMe.API
+{
+    public class Class
+    {
+    }
+}
--- VoteMe.API/Program.cs
+++ VoteMe.API/Program.cs
@@ -4,6 +4,10 @@ using VoteMe.Application.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile("appsettings.Local.json", optional: true);
+
 builder.Services.AddApiServices();
 builder.Services.AddVoteMeInfrastructure(builder.Configuration);
 builder.Services.AddApplicationServices();
